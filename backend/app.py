651017e88f2673426a76a8cedabcb93d3351538a
talhaw-cmd <123mtalha789@gmail.com>
2026-06-27 02:37:28 +0500

    AI-analyzer

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+                   "https://ai-resume-analyzer-two-hazel.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
